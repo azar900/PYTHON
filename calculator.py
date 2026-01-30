c0bd34c582d9adf36ce186e1406bf807671b3da9
azar <mohamedazarudeen049@gmail.com>
2026-01-30 20:28:10 +0530

    some initial calculator

---
+++ calculator.py
@@ -0,0 +1,35 @@
+# addition
+number_1 = int(input("enter the number 1"))
+number_2 = int(input("enter the number 2"))
+result = number_1 + number_2
+
+print("the result is", number_1 + number_2) 
+
+
+# subtraction
+number_1 = int(input("enter the number 1"))
+number_2 = int(input("enter the number 2"))
+result = number_1 - number_2
+
+print("the result is", number_1 - number_2) 
+
+
+
+
+# multify
+number_1 = int(input("enter the number 1"))
+number_2 = int(input("enter the number 2"))
+result = number_1 * number_2
+
+print("the result is", number_1 * number_2) 
+
+
+
+
+
+# divi
+number_1 = int(input("enter the number 1"))
+number_2 = int(input("enter the number 2"))
+result = number_1 / number_2
+
+print("the result is", number_1 / number_2) 
