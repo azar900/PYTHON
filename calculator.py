64f0dc93cb097b085d836be7dbba983701bf6890
azar <mohamedazarudeen049@gmail.com>
2026-01-31 15:05:00 +0530

    finish - calculator app

--- calculator.py
+++ calculator.py
@@ -1,35 +1,77 @@
-# addition
-number_1 = int(input("enter the number 1"))
-number_2 = int(input("enter the number 2"))
-result = number_1 + number_2
-
-print("the result is", number_1 + number_2) 
 
 
-# subtraction
-number_1 = int(input("enter the number 1"))
-number_2 = int(input("enter the number 2"))
-result = number_1 - number_2
+# get user intention
+print("""
+      1.addition \n
+      2.subtraction \n
+      3.muliplication \n
+      4.division \n
+      5.press 5 to exit
 
-print("the result is", number_1 - number_2) 
+""")
 
+calucaltor_condition = True
 
+while calucaltor_condition:
+    print("\n")
+    print("welcome to calculator !")
 
-
-# multify
-number_1 = int(input("enter the number 1"))
-number_2 = int(input("enter the number 2"))
-result = number_1 * number_2
-
-print("the result is", number_1 * number_2) 
+    user_operation = int(input("enter your option : "))
 
 
 
+    if user_operation == 1:
+    
+        # addition
+        print("addition")
+        number_1 = int(input("enter the number 1"))
+        number_2 = int(input("enter the number 2"))
+        result = number_1 + number_2
+
+        print("the result is", result) 
 
 
-# divi
-number_1 = int(input("enter the number 1"))
-number_2 = int(input("enter the number 2"))
-result = number_1 / number_2
 
-print("the result is", number_1 / number_2) 
+    elif user_operation == 2:
+    
+        # subtraction
+        print("subtraction")
+        number_1 = int(input("enter the number 1"))
+        number_2 = int(input("enter the number 2"))
+        result = number_1 - number_2
+
+        print("the result is", result) 
+
+
+
+
+    elif user_operation == 3:
+    
+        # multify
+        print("multiplication")
+        number_1 = int(input("enter the number 1"))
+        number_2 = int(input("enter the number 2"))
+        result = number_1 * number_2
+
+        print("the result is", result) 
+
+
+
+
+ 
+    elif user_operation == 4:
+
+        # divi
+        print("division")
+        number_1 = int(input("enter the number 1"))
+        number_2 = int(input("enter the number 2"))
+        result = number_1 / number_2
+        print("the result is", result) 
+
+    elif user_operation == 5:
+        print("thanks for using calculator, see you again. ")
+        calucaltor_condition = False
+
+
+
+
